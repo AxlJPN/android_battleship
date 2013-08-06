f9164b703f990b9a9c1cfc0b1b6a8cc4bf8c59c9
N.Wada <greenday8711526@gmail.com>
2013-08-06 16:33:31 +0900

    Movement()とAttackRolls()内の処理を一部追加

--- src/com/example/battleship/BattleshipClass.java
+++ src/com/example/battleship/BattleshipClass.java
@@ -3,6 +3,7 @@ package com.example.battleship;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.battleship.code.AttackResult;
 import com.example.battleship.code.ShipType;
 
 public class BattleshipClass {
@@ -75,19 +76,58 @@ public class BattleshipClass {
      * @param pointX
      * @param pointY
      */
-    public void Movement(int pointX, int pointY) {
+    public void Movement(int pointX, int pointY, ShipType shipType) {
+        ships.get(shipType).PositionX = pointX;
+        ships.get(shipType).PositionY = pointY;
+        
+        // TODO ListViewにログ表示
+        // 「XYへ【種類】が移動」 的な
     }
 
     /**
      * 攻撃判定
+     * ※複数のターゲットの近くに当たった場合はどうするか
+     * (下図の1～4、中心の●に当たった場合)
+     * ● = ターゲット
+     * ○ = 空き
+     * ○12
+     * ○●●
+     * ○34
+     * 
      * 
      * @param pointX
      * @param pointY
      * @param attackPower
      * @return
      */
-    public int AttackRolls(int pointX, int pointY, int attackPower) {
+    public AttackResult AttackRolls(int pointX, int pointY, int attackPower) {
+        AttackResult ret = AttackResult.FAIL;
+        
+        for(ShipType type : ships.keySet()){
+            int x = ships.get(type).PositionX;
+            int y = ships.get(type).PositionY;
+            
+            if(x == pointX && y == pointY){
+                // X軸、Y軸ともにどんぴしゃの場合
+                // HPを減らす
+                ships.get(type).HitPoint -= attackPower;
+                
+                // TODO ListViewにログを表示
+                // 「命中！」など？
 
-        return 0;
+                if(ret != AttackResult.HIT)
+                    ret = AttackResult.HIT;
+            }
+            else if(x+1 == pointX || x-1 == pointX || y+1 == pointY || y-1 == pointY){
+                // X軸±1、Y軸±1の場合
+                // TODO ListViewにログを表示
+                // 「【種類】、波高し」、「【種類】、水しぶき」など
+                
+                if(ret == AttackResult.FAIL)
+                    ret = AttackResult.NEAR;
+            }
+        }
+
+        return ret;
     }
 }
