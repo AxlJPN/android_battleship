8b3b433fbe4ad95d5ec845bbffe9b3d1f447e42f
T.Iga <c.tricoides@gmail.com>
2013-08-06 16:00:29 +0900

    JavaDocを追加

--- src/com/example/battleship/code/AttackResult.java
+++ src/com/example/battleship/code/AttackResult.java
@@ -1,7 +1,13 @@
 package com.example.battleship.code;
 
+/**
+ * 攻撃結果
+ * 
+ * @author N.Wada
+ *
+ */
 public enum AttackResult {
-	HIT,
-	FAIL,
-	NEAR;
+	HIT,   // 当たった
+	FAIL,  // 外れた
+	NEAR   // 近くに落ちた
 }
--- src/com/example/battleship/code/ShipType.java
+++ src/com/example/battleship/code/ShipType.java
@@ -1,7 +1,14 @@
 package com.example.battleship.code;
 
+/**
+ * 
+ * 船の種類
+ * 
+ * @author N.Wada
+ *
+ */
 public enum ShipType {
-	BATTLESHIP,
-	DESTROYER,
-	SUBMARINE
+	BATTLESHIP,    // 戦艦
+	DESTROYER,     // 駆逐艦
+	SUBMARINE      // 潜水艦
 }
