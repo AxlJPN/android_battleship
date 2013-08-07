9752811e88ba4537f2d9e0c6cc93fc4a9b82900f
T.Iga <c.tricoides@gmail.com>
2013-08-07 15:25:33 +0900

    定数クラスを追加

---
+++ src/com/example/battleship/Common.java
@@ -0,0 +1,11 @@
+package com.example.battleship;
+
+public interface Common {
+
+    // 定数
+    // ListViewのID
+    static final int LISTVIEWID = 100;
+    static final int WIDTH = 5;
+    static final int HEIGHT = 5;
+
+}
