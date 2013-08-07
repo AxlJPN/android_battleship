6b50e5b1d943b5866bd1c60d1b71ada38a31060e
T.Iga <c.tricoides@gmail.com>
2013-08-07 15:25:14 +0900

    Commonをimplementsするように修正

--- src/com/example/battleship/BattleShip.java
+++ src/com/example/battleship/BattleShip.java
@@ -7,166 +7,174 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TableRow.LayoutParams;
 
-import com.example.battleship.code.ShipType;
-
-public class BattleShip extends CommActivity {
+public class BattleShip extends CommActivity implements Common {
 
     // 変数
-	BattleshipClass _battleShip = null;
-	private ArrayAdapter<Ship> _adapter;
-	private AlertDialog _alertDialog;
-	private int _selectedIndex;
-	private Button _selectedButton;
-	private ArrayList<ArrayList<Integer>> _btnIDs;
-	
-	// 定数
-	// ListViewのID
-	private final int LISTVIEWID = 100;
-    private final int WIDTH = 5;
-    private final int HEIGHT = 5;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_battle_ship);
+    BattleshipClass _battleShip = null;
+    private int _selectedIndex;
+    private Button _selectedButton;
+    private ArrayList<ArrayList<Integer>> _btnIDs;
 
-		// 端末接続設定
-		connectionSetting();
-		
-		_btnIDs = new ArrayList<ArrayList<Integer>>();
-		
-		// マス作成
-		this.SetButtons(WIDTH, HEIGHT);
-		this.SetListView(WIDTH);
-		
-		_battleShip = new BattleshipClass();
-		_adapter = new ArrayAdapter<Ship>(this, android.R.layout.simple_list_item_single_choice);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_ship);
 
-		_adapter.add(new Ship(ShipType.BATTLESHIP, "戦艦"));
-		_adapter.add(new Ship(ShipType.DESTROYER, "駆逐艦"));
-		_adapter.add(new Ship(ShipType.SUBMARINE, "潜水艦"));
-		
-	}
-	
-	/**
-	 * ボタンを動的に配置
-	 * @param  x
-	 * @param  y
-	 */
-	private void SetButtons(int x, int y){
-		int id = 0;
-		TableLayout layout = (TableLayout)findViewById(R.id.TableLayout1);
-		
-		for(int i = 0; i < x; i++){
-			TableRow row = new TableRow(this);
-			ArrayList<Integer> aryList = new ArrayList<Integer>();
-			for(int j = 0; j < y; j++){
-				// ボタンを生成
-				Button button = new Button(this);
-				// IDを設定
-				button.setId(id);
-				button.setText("");
+        // 端末接続設定
+        connectionSetting();
 
-				row.addView(button);
+        _btnIDs = new ArrayList<ArrayList<Integer>>();
 
-				// ボタンイベントを追加
-				button.setOnClickListener(new OnClickButtonWhenFirst());
+        // マス作成
+        this.SetButtons(WIDTH, HEIGHT);
+        this.SetListView(WIDTH);
 
-				
-				// ボタンIDを取得
-				aryList.add(id);
-				
-				// IDをインクリメント
-				id++;
-			}
+        _battleShip = new BattleshipClass();
+    }
+
+    /**
+     * ボタンを動的に配置
+     * 
+     * @param x
+     * @param y
+     */
+    private void SetButtons(int x, int y) {
+        int id = 0;
+        TableLayout layout = (TableLayout) findViewById(R.id.TableLayout1);
+
+        for (int i = 0; i < x; i++) {
+            TableRow row = new TableRow(this);
+            ArrayList<Integer> aryList = new ArrayList<Integer>();
+            for (int j = 0; j < y; j++) {
+                // ボタンを生成
+                Button button = new Button(this);
+                // IDを設定
+                button.setId(id);
+                button.setText("");
+
+                row.addView(button);
+
+                // ボタンイベントを追加
+                button.setOnClickListener(new OnClickButtonWhenFirst());
+
+                // ボタンIDを取得
+                aryList.add(id);
+
+                // IDをインクリメント
+                id++;
+            }
             _btnIDs.add(aryList);
-			layout.addView(row);
-		}
-	}
+            layout.addView(row);
+        }
+    }
 
-	/**
-	 * リストビューを追加する
-	 * @param x
-	 */
-	private void SetListView(int x){
-		TableLayout layout = (TableLayout)findViewById(R.id.TableLayout1);
-		TableRow row = new TableRow(this);
+    /**
+     * リストビューを追加する
+     * 
+     * @param x
+     */
+    private void SetListView(int x) {
+        TableLayout layout = (TableLayout) findViewById(R.id.TableLayout1);
+        TableRow row = new TableRow(this);
 
-		LayoutParams param = new LayoutParams();
-		param.span = x;
+        LayoutParams param = new LayoutParams();
+        param.span = x;
 
-		// ListView作成
-		ListView list = new ListView(this);
-		list.setId(LISTVIEWID);
-		
-		row.addView(list, param);
-		layout.addView(row);
-	}
-	
-	/**
-	 * 選択されたテキストと同じ物を見つけてクリアする
-	 * @param selectText
-	 */
-	private void ClearButtonText(String selectText){
-	    // ボタンを一個ずつ見て、同じ文字が存在するか確認
-	    for(ArrayList<Integer> btnId : _btnIDs){
-	        for(int id : btnId)
-	        {
-    	        Button button = (Button)findViewById(id);
-    	        if(button.getText().toString().equals(selectText)){
-    	            // 同じ文字が見つかったらクリアする
-    	            button.setText("");
-    	            break;
-    	        }
-	        }
-	    }
-	}
+        // ListView作成
+        ListView list = new ListView(this);
+        list.setId(LISTVIEWID);
 
+        row.addView(list, param);
+        layout.addView(row);
+    }
 
-	private class OnClickButtonWhenFirst implements OnClickListener{
+    /**
+     * 選択されたテキストと同じ物を見つけてクリアする
+     * 
+     * @param selectText
+     */
+    public void ClearButtonText(String selectText) {
+        // ボタンを一個ずつ見て、同じ文字が存在するか確認
+        for (ArrayList<Integer> btnId : _btnIDs) {
+            for (int id : btnId) {
+                Button button = (Button) findViewById(id);
+                if (button.getText().toString().equals(selectText)) {
+                    // 同じ文字が見つかったらクリアする
+                    button.setText("");
+                    break;
+                }
+            }
+        }
+    }
 
-		@Override
-		public void onClick(View v) {
-			_selectedButton = (Button)findViewById(v.getId());
-			
-			AlertDialog.Builder builder = new AlertDialog.Builder(BattleShip.this);
-			builder.setTitle("配置する船の選択");
-			builder.setSingleChoiceItems(_adapter, _selectedIndex, onDialogClickListener);
-			_alertDialog = builder.create();
-			_alertDialog.show();
-			
-			_selectedButton.setOnClickListener(new OnClickButtonGameStart());
-		}
-	}
-	
-	/**
-	 * ゲーム開始後のボタンクリックイベント
-	 * @author N.Wada
-	 *
-	 */
-	private class OnClickButtonGameStart implements OnClickListener{
+    private class OnClickButtonWhenFirst implements OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            _selectedButton = (Button) findViewById(v.getId());
+
+            ClearButtonText(shortName);
+            _selectedButton.setText(shortName);
+
+            // 配置されている船の数を取得
+            int shipCount = getShipCount();
+
+            if (shipCount < 3) {
+                _alertDialog = createSelectShipDialog(BattleShip.this);
+                _alertDialog.show();
+            } else {
+                for (int i = 0; i < WIDTH * HEIGHT; i++) {
+                    ((Button) findViewById(i)).setOnClickListener(new OnClickButtonGameStart());
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 配置されている船の数を返す
+     * 
+     * @return 配置されている船の数
+     */
+    public int getShipCount() {
+        int shipCount = 0;
+        for (int i = 0; i < Common.WIDTH * Common.HEIGHT; i++) {
+            Button button = (Button) findViewById(i);
+            if (button.getText().toString().length() > 0) {
+                shipCount++;
+            }
+        }
+        return shipCount;
+    }
+
+    /**
+     * ゲーム開始後のボタンクリックイベント
+     * 
+     * @author N.Wada
+     * 
+     */
+    private class OnClickButtonGameStart implements OnClickListener {
 
         @Override
         public void onClick(final View v) {
             // ボタンのテキストが何もない場合、何もしない
-            Button button = (Button)findViewById(v.getId());
-            if(button.getText().toString().equals("")){
+            Button button = (Button) findViewById(v.getId());
+            if (button.getText().toString().equals("")) {
                 return;
             }
-            
+
             // 行動を選択させる
             AlertDialog.Builder builder = new AlertDialog.Builder(BattleShip.this);
             builder.setTitle("行動選択");
             builder.setMessage("攻撃／移動どちらを行いますか？");
             builder.setNegativeButton("攻撃", new DialogInterface.OnClickListener() {
-                
+
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     // TODO 自分を中心としたマスを赤くする
@@ -182,7 +190,7 @@ public class BattleShip extends CommActivity {
                 }
             });
             builder.setNeutralButton("移動", new DialogInterface.OnClickListener() {
-                
+
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     // TODO 自分の上下左右のマスを全て青くする
@@ -196,19 +204,19 @@ public class BattleShip extends CommActivity {
             builder.setPositiveButton("キャンセル", null);
             builder.show();
         }
-	    
-	}
 
-	private DialogInterface.OnClickListener onDialogClickListener = new DialogInterface.OnClickListener() {
-        
+    }
+
+    private DialogInterface.OnClickListener onDialogClickListener = new DialogInterface.OnClickListener() {
+
         @Override
         public void onClick(DialogInterface dialog, int which) {
             String selectText = _adapter.getItem(which).getShipName();
             ClearButtonText(selectText);
-            
+
             _selectedIndex = which;
             _selectedButton.setText(selectText);
             _alertDialog.dismiss();
         }
-	};
+    };
 }
