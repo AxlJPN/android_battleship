7e4c4e19315a57c9069a96708ec1841b5aba8abd
T.Iga <c.tricoides@gmail.com>
2013-08-07 11:24:04 +0900

    javadocを追加

--- src/com/example/battleship/Ship.java
+++ src/com/example/battleship/Ship.java
@@ -2,41 +2,83 @@ package com.example.battleship;
 
 import com.example.battleship.code.ShipType;
 
+/**
+ * 船クラス
+ * 
+ * @author T.Iga
+ * 
+ */
 public class Ship {
 
+    /**
+     * 船の種類
+     */
     private ShipType shipType;
+    /**
+     * 船の名前
+     */
     private String shipName;
+    /**
+     * 船の短い名前
+     */
     private String shortShipName;
 
-    public Ship(){
-        
+    /**
+     * デフォルトコンストラクタ
+     */
+    public Ship() {
+
     }
-    
-    public Ship(ShipType shipType, String shipName){
+
+    /**
+     * コンストラクタ
+     * 
+     * @param shipType
+     *            船の種類
+     * @param shipName
+     *            船の名前
+     */
+    public Ship(ShipType shipType, String shipName) {
         this.shipType = shipType;
         this.shipName = shipName;
     }
-    public Ship(ShipType shipType, String shipeName, String shortShipName) {
+
+    /**
+     * コンストラクタ
+     * 
+     * @param shipType
+     *            船の種類
+     * @param shipName
+     *            船の名前
+     * @param shortShipName
+     *            船の短い名前
+     */
+    public Ship(ShipType shipType, String shipName, String shortShipName) {
         this.shipType = shipType;
         this.shipName = shipName;
-	this.shortShipName = shortShipName;
+        this.shortShipName = shortShipName;
     }
-    
+
     public ShipType getShipType() {
         return shipType;
     }
+
     public void setShipType(ShipType shipType) {
         this.shipType = shipType;
     }
+
     public String getShipName() {
         return shipName;
     }
+
     public void setShipName(String shipName) {
         this.shipName = shipName;
     }
+
     public String getShortShipName() {
         return this.shortShipName;
     }
+
     public void setShortShipName(String shortShipName) {
         this.shortShipName = shortShipName;
     }
