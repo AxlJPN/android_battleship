2ddd2c48be18c63f0b98b0b1d31c566232eeca37
T.Iga <c.tricoides@gmail.com>
2013-08-07 11:25:16 +0900

    タブをスペースに変換

--- src/com/example/battleship/code/ShipType.java
+++ src/com/example/battleship/code/ShipType.java
@@ -5,10 +5,10 @@ package com.example.battleship.code;
  * 船の種類
  * 
  * @author N.Wada
- *
+ * 
  */
 public enum ShipType {
-	BATTLESHIP,    // 戦艦
-	DESTROYER,     // 駆逐艦
-	SUBMARINE      // 潜水艦
+    BATTLESHIP, // 戦艦
+    DESTROYER,  // 駆逐艦
+    SUBMARINE   // 潜水艦
 }
