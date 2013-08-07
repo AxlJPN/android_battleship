11bbfb77e3eee79eb4339e5dfb39b7264881717e
T.Iga <c.tricoides@gmail.com>
2013-08-07 15:17:32 +0900

    船配置処理を追加

--- src/com/example/battleship/CommActivity.java
+++ src/com/example/battleship/CommActivity.java
@@ -1,30 +1,34 @@
 package com.example.battleship;
 
-import com.example.battleship.CommModule;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.battleship.code.ShipType;
+
 /**
  * 初期接続設定クラス
- *
+ * 
  * @author T.Sasaki
- *
+ * 
  */
 public class CommActivity extends Activity {
     Context context = null;
     int port = 8080;
     String serverIpAddress = null;
     String clientIpAddress = null;
-    
+
     CommModule comm = null;
+
+    protected String shortName = null;
+    protected ArrayAdapter<Ship> _adapter;
+    protected AlertDialog _alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +38,15 @@ public class CommActivity extends Activity {
 
     /**
      * 初期接続の受信
+     * 
      * @author T.Sasaki
-     *
+     * 
      */
     public class connectRecieve extends CommModule.Recieve {
         public connectRecieve(CommModule commModule, Context con) {
             commModule.super(con);
         }
-        
+
         @Override
         protected String doInBackground(String... args0) {
             return super.doInBackground(args0);
@@ -58,34 +63,38 @@ public class CommActivity extends Activity {
 
     /**
      * 初期接続の送信
+     * 
      * @author T.Sasaki
-     *
+     * 
      */
     public class connectSend extends CommModule.Send {
         public connectSend(CommModule commModule, Context con, String serverIp) {
             commModule.super(con, serverIp);
         }
+
         @Override
         protected Boolean doInBackground(String... args0) {
             return super.doInBackground("1");
         }
+
         @Override
         protected void onPostExecute(Boolean bol) {
             super.onPostExecute(bol);
-            if(bol)
+            if (bol) {
                 Toast.makeText(context, "送信完了", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
     /*
      * 接続設定画面表示
      */
-    public void connectionSetting(){
+    public void connectionSetting() {
 
         comm = new CommModule(context);
-        
+
         // 接続確認
-        if(comm.getClientIpAddress() == null || comm.getServerIpAddress() == null)
+        if (comm.getClientIpAddress() == null || comm.getServerIpAddress() == null)
             this.showSelectServerCliendDialog();
     }
 
@@ -119,6 +128,16 @@ public class CommActivity extends Activity {
         builder.show();
     }
 
+    class onSelectShipDialogClickListener implements OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+
+            shortName = _adapter.getItem(which).getShortShipName();
+            dialog.dismiss();
+        }
+    }
+
     /*
      * サーバー側接続画面表示
      */
@@ -129,7 +148,7 @@ public class CommActivity extends Activity {
         builder.setTitle("Connection destination(Server)");
 
         // IP取得
-        builder.setMessage("Please wait.\nYour IP is: " +comm.getWifiInfo());
+        builder.setMessage("Please wait.\nYour IP is: " + comm.getWifiInfo());
 
         builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
             @Override
@@ -148,13 +167,14 @@ public class CommActivity extends Activity {
     /*
      * クライアント側接続画面表示
      */
-    private void showSendDialog(){
+    private void showSendDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setIcon(R.drawable.ic_launcher);
         builder.setTitle("Connection destination(Client)");
 
-        builder.setMessage("Please specify the connection destination.\nYour IP is: " + comm.getWifiInfo());
+        builder.setMessage("Please specify the connection destination.\nYour IP is: "
+                + comm.getWifiInfo());
         final EditText editView = new EditText(this);
         builder.setView(editView);
 
@@ -164,6 +184,10 @@ public class CommActivity extends Activity {
             public void onClick(DialogInterface dialog, int which) {
                 // サーバー側に接続する
                 new connectSend(comm, context, editView.getText().toString()).execute();
+
+                // 船を配置するダイアログを表示
+                _alertDialog = createSelectShipDialog(CommActivity.this);
+                _alertDialog.show();
             }
         });
 
@@ -178,5 +202,22 @@ public class CommActivity extends Activity {
         });
 
         builder.show();
+    }
+    
+    protected AlertDialog createSelectShipDialog(Context context) {
+        AlertDialog dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("配置する船の選択");
+
+        _adapter = new ArrayAdapter<Ship>(context,
+                android.R.layout.simple_list_item_single_choice);
+
+        _adapter.add(new Ship(ShipType.BATTLESHIP, "戦艦", "B"));
+        _adapter.add(new Ship(ShipType.DESTROYER, "駆逐艦", "D"));
+        _adapter.add(new Ship(ShipType.SUBMARINE, "潜水艦", "S"));
+
+        builder.setSingleChoiceItems(_adapter, 0, new onSelectShipDialogClickListener());
+        dialog = builder.create();
+        return dialog;
     }
 }
